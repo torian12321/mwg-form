440cee849ea05f96824096199aca9297ef6232f1
Aitor palomares <aitor.palomares@hmhco.com>
2018-03-12 09:24:50 +0000

    loading page

--- src/views/step_03.jsx
+++ src/views/step_03.jsx
@@ -2,23 +2,49 @@ import React from 'react';
 import './_style.scss';
 
 import {
-  Button, Panel,
+  Panel, Loading,
   Container, Row, Col
 
 } from 'export';
 
-const FormStep_03 = props =>(
-  <Container>
 
-    <Row>
-      <Col xs={8} xsOffset={2} sm={6} smOffset={2} md={4} smOffset={4}>
-        <Panel theme='sec' className='panelThanks'>
-          <h1>Thank You</h1>
-        </Panel>
-      </Col>
-    </Row>
+class FormStep_03 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading : true
+    };
+  }
 
-  </Container>
-);
+  componentDidMount(){
+    setTimeout(this.isUserValid.bind(this), 3000);
+  }
+
+  isUserValid() {
+    // Emulate delay response from server
+    this.setState({
+      isLoading: false
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+
+        <Row>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={2} md={4} smOffset={4}>
+            <Panel theme='sec' className='panelThanks'>
+              {this.state.isLoading ?
+                <Loading /> :
+                <h1>Thank You</h1>
+              }
+            </Panel>
+          </Col>
+        </Row>
+
+      </Container>
+    )
+  }
+}
 
 module.exports = FormStep_03;

2ad6b4d82b51f20cf081f87e8be2ff4e18006916
Aitor palomares <aitor.palomares@hmhco.com>
2018-03-12 09:08:19 +0000

    validate fields

--- src/views/step_02.jsx
+++ src/views/step_02.jsx
@@ -8,43 +8,88 @@ import {
 
 } from 'export';
 
-const FormStep_02 = props =>(
-  <Container className='containerForm'>
+class FormStep_02 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name       : { isValid: false, value  : '' },
+      surname    : { isValid: false, value  : '' },
+      birthday   : { isValid: false, value  : '' },
+      nacionality: { isValid: false, value  : '' },
+      mail       : { isValid: false, value  : '' },
+      ocupation  : { isValid: false, value  : '' },
+      validForm  : true // Should start on false, is true for testing porpouse
+    };
+  }
 
-    <Row>
-      <Col xs={12}>
-        <Panel theme='sec'>
+  validateName(inputVal) {
+    // TODO: do some kind of validation
+    const name = {
+      value  : inputVal,
+      isValid: true
+    }
+    this.setState({ name });
+    this.validateForm();
+  }
 
-          <Row>
-            <Col xs={12} md={6}>
-              <Input label='First Name' placeholder='John' state='success'/>
-            </Col>
-            <Col xs={12} md={6}>
-              <Input label='Surname' placeholder='Smith' />
-            </Col>
-            <Col xs={12} md={6}>
-              <Input label='Date of birth' placeholder='01/01/1980' />
-            </Col>
-            <Col xs={12} md={6}>
-              <Input label='Naionality' placeholder='Irish' />
-            </Col>
-            <Col xs={12} md={6} state>
-              <Input label='Email Address' placeholder='hello@info.com' state='error' info='Specific Error Feedback Text: located where the input is'/>
-            </Col>
-            <Col xs={12} md={6}>
-              <Input label='Ocupation' placeholder='Front End Developer' />
-            </Col>
-          </Row>
+  // TODO: repeat 'validateName' for rest of fields, applying differen validation criteria
+  // ....
+  // ....
 
-          <div className='panel__btnArea'>
-            <Button className='right'>Submit</Button>
-          </div>
-        </Panel>
-      </Col>
-    </Row>
+  validateForm(){
+    // Enable/Disable button
+    const isValid =
+      name.isValid        &&
+      surname.isValid     &&
+      birthday.isValid    &&
+      nacionality.isValid &&
+      mail.isValid        &&
+      ocupation.isValid;
 
-  </Container>
-);
+      // this.setState({ validForm: isValid });
+      this.setState({ validForm: true });         // For testing, returns alway true
+  }
+render() {
+
+  return (
+    <Container className='containerForm'>
+
+      <Row>
+        <Col xs={12}>
+          <Panel theme='sec'>
+
+            <Row>
+              <Col xs={12} md={6}>
+                <Input value={this.state.name.value} onChange={this.validateName.bind(this)} label='First Name' placeholder='John' state='success'/>
+              </Col>
+              <Col xs={12} md={6}>
+                <Input value={this.state.surname.value} label='Surname' placeholder='Smith' />
+              </Col>
+              <Col xs={12} md={6}>
+                <Input value={this.state.name.birthday} label='Date of birth' placeholder='01/01/1980' />
+              </Col>
+              <Col xs={12} md={6}>
+                <Input value={this.state.name.naionality} label='Naionality' placeholder='Irish' />
+              </Col>
+              <Col xs={12} md={6} state>
+                <Input value={this.state.name.email} label='Email Address' placeholder='hello@info.com' state='error' info='Specific Error Feedback Text: located where the input is'/>
+              </Col>
+              <Col xs={12} md={6}>
+                <Input value={this.state.name.ocupation} label='Ocupation' placeholder='Front End Developer' />
+              </Col>
+            </Row>
+
+            <div className='panel__btnArea'>
+              <Button className='right' disabled={!this.state.validForm}>Submit</Button>
+            </div>
+          </Panel>
+        </Col>
+      </Row>
+
+    </Container>
+    )
+  }
+};
 
 
 module.exports = FormStep_02;
